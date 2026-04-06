ffee47fecdeaa005d417fac266526aebc3dbd19c
Thao Ho Phuong <138818588+hphthao1705@users.noreply.github.com>
2026-04-06 11:21:58 +0700

    feat: apply AppDimension for HomeLayout

---
+++ app/src/main/java/com/example/weather_app/util/AppDimension.kt
@@ -0,0 +1,13 @@
+package com.example.weather_app.util
+
+import androidx.compose.ui.unit.dp
+
+object AppDimension {
+    val dimension_8 = 8.dp
+    val dimension_15 = 15.dp
+    val dimension_20 = 20.dp
+    val dimension_26 = 26.dp
+    val dimension_33 = 33.dp
+    val dimension_43 = 43.dp
+    val dimension_50 = 50.dp
+}
